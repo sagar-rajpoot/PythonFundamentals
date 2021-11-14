f09bd6e5b91335dcfdcfc55c6fbae544d3d0cb84
Sagar-rajpoot <sagarsinghrajpoot@Sagars-MacBook-Pro.local>
2021-11-15 00:16:09 +0100

    Adding if Operation in Python

---
+++ if.py
@@ -0,0 +1,28 @@
+a= False
+b= True
+
+if b:
+    print("I am in if")
+else:
+    print("I am in else")
+
+
+if a and b:
+    print("I am in if")
+else:
+    print("I am in else")
+
+if a or b:
+    print("I am in if")
+else:
+    print("I am in else")
+
+
+
+
+
+
+
+
+
+
