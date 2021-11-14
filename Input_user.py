367c8030c6452649ef97cc8f0acac5a980dda5d2
Sagar-rajpoot <sagarsinghrajpoot@Sagars-MacBook-Pro.local>
2021-11-15 00:30:52 +0100

    Imput from User for some operation

---
+++ Input_user.py
@@ -0,0 +1,9 @@
+Num1 = input("Please enter first number: ")
+Num2 = input("Please enter second number: ")
+total1 = int(Num1)+int(Num2)
+print(total1)
+print("Num1 + Num2 =" + str(total1))
+
+#  Below also looks same here
+total2 = Num1+Num2
+print(total2)
