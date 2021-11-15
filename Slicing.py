e078b95370cbb5636a440da4a34f9fef9ad8dd5f
Sagar-rajpoot <sagarsinghrajpoot@Sagars-MacBook-Pro.local>
2021-11-16 00:10:21 +0100

    Slicing in Python

---
+++ Slicing.py
@@ -0,0 +1,22 @@
+x="unhappy"
+
+print(x[2:])
+
+print(x[2:4])
+
+y="12345678"
+
+print(y[::1])
+
+# print alternative Numbers ( Move 2 step at a time )
+
+print(y[::2])
+
+
+# Move from last 
+
+print(y[::-1])
+
+
+
+
