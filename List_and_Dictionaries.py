d9c7f43ed02f5e405e4e3ed1bb2c9bc617ae97ee
Sagar-rajpoot <sagarsinghrajpoot@Sagars-MacBook-Pro.local>
2021-11-16 00:35:45 +0100

    Playing with Python List- Insert-delete by index

--- List_and_Dictionaries.py
+++ List_and_Dictionaries.py
@@ -4,7 +4,6 @@ for a in Friends:
     print(a)
 
 #nested list
-
 mylist = [
     [1,2,3],
     [4,5,6],
@@ -22,3 +21,14 @@ mylist=[1,2,3,4]
 print(mylist)
 mylist.append(5)
 print(mylist)
+
+# You can enter anything at any index - Rest will pushed to next indexes 
+mylist.insert(0,'python')
+print(mylist)
+
+# Similarly You can delete also 
+del mylist[0]
+print(mylist)
+
+#I want get data from list Starting from index 1 till last and each time want to move 2 steps at a time
+print(mylist[0::2])
