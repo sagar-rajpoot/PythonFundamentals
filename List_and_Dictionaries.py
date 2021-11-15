6a6d144d1451bd162fe6f9d2ccf2e22acdb8ed62
Sagar-rajpoot <sagarsinghrajpoot@Sagars-MacBook-Pro.local>
2021-11-15 21:51:14 +0100

    List and Dictionaries

---
+++ List_and_Dictionaries.py
@@ -0,0 +1,20 @@
+Friends=["sagar","sandeep","Munnu"]
+
+for a in Friends:
+    print(a)
+
+#nested list
+
+mylist = [
+    [1,2,3],
+    [4,5,6],
+    [7,8]
+]
+
+print(mylist[0])
+print(mylist[0][1])
+
+for a in mylist:
+    for b in a:
+        print(b)
+
