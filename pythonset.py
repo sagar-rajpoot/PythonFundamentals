3888750c38c3a81845a8937644b6b0a8312d88c0
Sagar-rajpoot <sagarsinghrajpoot@Sagars-MacBook-Pro.local>
2021-11-24 01:18:47 +0100

    Adding Python Set

---
+++ pythonset.py
@@ -0,0 +1,17 @@
+data={1,5,'A',10.75,89,5}
+
+print(data)
+# there are two 5 , one 5 is eliminated in output
+# Set is Unordered collection so in output it is not in same order as we defined it at line1 - Thats y
+#set dont support index based operations
+
+data.add('aws')
+print(data)
+
+for x in data:
+    print(x)
+# you can print a set same like list
+
+
+
+
